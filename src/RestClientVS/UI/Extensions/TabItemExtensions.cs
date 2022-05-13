49d1c0ce9ac48f7a999122ca23a8a721775ad9dc
Andreas Müller <taori@users.noreply.github.com>
2022-05-13 12:57:19 +0200

    Style completion

---
+++ src/RestClientVS/UI/Extensions/TabItemExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace RestClientVS.UI.Extensions;
+
+public class TabItemExtensions
+{
+	public static readonly DependencyProperty HoverTabTemplateProperty = DependencyProperty.RegisterAttached(
+		"HoverTabTemplate", typeof(DataTemplate), typeof(TabItemExtensions), new PropertyMetadata(default(DataTemplate)));
+
+	[AttachedPropertyBrowsableForType(typeof(TabItem))]
+	public static void SetHoverTabTemplate(DependencyObject element, DataTemplate value)
+	{
+		element.SetValue(HoverTabTemplateProperty, value);
+	}
+
+	[AttachedPropertyBrowsableForType(typeof(TabItem))]
+	public static DataTemplate GetHoverTabTemplate(DependencyObject element)
+	{
+		return (DataTemplate)element.GetValue(HoverTabTemplateProperty);
+	}
+
+	public static readonly DependencyProperty SelectedTabTemplateProperty = DependencyProperty.RegisterAttached(
+		"SelectedTabTemplate", typeof(DataTemplate), typeof(TabItemExtensions), new PropertyMetadata(default(DataTemplate)));
+
+	[AttachedPropertyBrowsableForType(typeof(TabItem))]
+	public static void SetSelectedTabTemplate(DependencyObject element, DataTemplate value)
+	{
+		element.SetValue(SelectedTabTemplateProperty, value);
+	}
+
+	[AttachedPropertyBrowsableForType(typeof(TabItem))]
+	public static DataTemplate GetSelectedTabTemplate(DependencyObject element)
+	{
+		return (DataTemplate)element.GetValue(SelectedTabTemplateProperty);
+	}
+
+	public static readonly DependencyProperty DefaultTabTemplateProperty = DependencyProperty.RegisterAttached(
+		"DefaultTabTemplate", typeof(DataTemplate), typeof(TabItemExtensions), new PropertyMetadata(default(DataTemplate)));
+
+	[AttachedPropertyBrowsableForType(typeof(TabItem))]
+	public static void SetDefaultTabTemplate(DependencyObject element, DataTemplate value)
+	{
+		element.SetValue(DefaultTabTemplateProperty, value);
+	}
+
+	[AttachedPropertyBrowsableForType(typeof(TabItem))]
+	public static DataTemplate GetDefaultTabTemplate(DependencyObject element)
+	{
+		return (DataTemplate)element.GetValue(DefaultTabTemplateProperty);
+	}
+}
